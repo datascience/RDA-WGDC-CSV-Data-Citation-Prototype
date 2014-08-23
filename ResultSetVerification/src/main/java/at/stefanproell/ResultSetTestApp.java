d5250b4119642a332fad0ee4fe1463b6295f8e23
stefanproell <info@stefanproell.at>
2014-08-23 16:47:57 +0200

    fixed

--- ResultSetVerification/src/main/java/at/stefanproell/ResultSetTestApp.java
+++ ResultSetVerification/src/main/java/at/stefanproell/ResultSetTestApp.java
@@ -78,6 +78,22 @@
  *    limitations under the License.
  */
 
+/*
+ * Copyright [2014] [Stefan Pröll]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package at.stefanproell;
 
 import at.stefanproell.ResultSetVerification.ResultSetVerificationAPI;
@@ -105,7 +121,7 @@ public class ResultSetTestApp {
 */
 
 
-        api.calculateHashFromCompleteTableServerSide("MSD100k", true);
+        api.calculateHashFromCompleteTableServerSide("MSD10k", true);
 
         //api.calculateHashFromCompleteTableServerSide("MSD10k", false);
         System.exit(0);
--- ResultSetVerification/src/main/java/at/stefanproell/ResultSetVerification/ResultSetVerificationAPI.java
+++ ResultSetVerification/src/main/java/at/stefanproell/ResultSetVerification/ResultSetVerificationAPI.java
@@ -78,6 +78,22 @@
  *    limitations under the License.
  */
 
+/*
+ * Copyright [2014] [Stefan Pröll]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package at.stefanproell.ResultSetVerification;
 
 import com.sun.rowset.CachedRowSetImpl;
@@ -340,11 +356,11 @@ public class ResultSetVerificationAPI {
         algorithms.add("MD5");
         algorithms.add("SHA-256");
         if (algorithms.contains(algorithm)) {
-            MessageDigest crypto = null;
+            this.crypto = null;
 
 
             try {
-                crypto = MessageDigest.getInstance(algorithm);
+                this.crypto = MessageDigest.getInstance(algorithm);
                 this.crypto.reset();
             } catch (NoSuchAlgorithmException e) {
                 e.printStackTrace();
