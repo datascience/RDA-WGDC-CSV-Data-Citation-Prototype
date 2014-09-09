e14c01fe1b20edd44d7fd899802fdd3b0eaa87d9
Stefan Proell <info@stefanproell.at>
2014-09-09 17:03:01 +0200

    deployment works

--- WebInterface/src/main/java/Bean/DatabaseTableNameBean.java
+++ WebInterface/src/main/java/Bean/DatabaseTableNameBean.java
@@ -46,6 +46,22 @@
  *    limitations under the License.
  */
 
+/*
+ * Copyright [2014] [Stefan Pröll]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package Bean;
 
 
@@ -56,6 +72,7 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
+import javax.faces.event.ValueChangeEvent;
 import java.io.Serializable;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -119,15 +136,27 @@ public class DatabaseTableNameBean implements Serializable {
         try {
 
             DatabaseTools dbtools = new DatabaseTools();
-            //databaseNames = dbtools.getAvailableDatabases();
-            databaseNames = new ArrayList<String>();
-            databaseNames.add("Test hard coedd");
+            databaseNames = dbtools.getAvailableDatabases();
+            //databaseNames = new ArrayList<String>();
+            //databaseNames.add("Test hard coedd");
 
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
+
+    }
+
+    /**
+     * React on change
+     *
+     * @param event
+     */
+    public void handleChange(ValueChangeEvent event) {
+        String selectedDB = event.getNewValue().toString();
+        this.logger.info("Databasename = " + selectedDB);
 
 
     }
