180ed4c3f3833f647a7f9e01469c251ad93dea49
Stefan Proell <info@stefanproell.at>
2014-09-10 11:06:39 +0200

    selection works

--- WebInterface/src/main/java/Bean/FileUploadController.java
+++ WebInterface/src/main/java/Bean/FileUploadController.java
@@ -30,6 +30,22 @@
  *    limitations under the License.
  */
 
+/*
+ * Copyright [2014] [Stefan Pröll]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package Bean;
 
 import org.primefaces.event.FileUploadEvent;
@@ -42,6 +58,7 @@ import javax.faces.context.FacesContext;
 import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.logging.Logger;
 
 /**
  * Created by stefan on 17.06.14.
@@ -50,13 +67,19 @@ import java.util.*;
 @ManagedBean
 @SessionScoped
 public class FileUploadController implements Serializable {
+    private Logger logger;
     private String tableName;
     private HashMap<String, String> filesList;
     private List<String> filesListStrings;
 
+    private String currentSessionType = "";
+
     public FileUploadController() {
         this.filesList = new HashMap<String, String>();
         this.filesListStrings = new ArrayList<String>();
+        this.currentSessionType = getUploadTypeFromSession();
+
+        this.logger = Logger.getLogger(this.getClass().getName());
 
 
     }
@@ -161,5 +184,20 @@ public class FileUploadController implements Serializable {
 
         System.out.println("SEEEEEEEEEEEEEESION " + filesList.size() + " table name " + this.tableName);
     }
+
+    /**
+     * Get the type of the upload. Available types are: newCSV, updateExistingCSV, appendNewRowsToExistingCSV.
+     * The information is stored in the session variable uploadSessionType
+     */
+    private String getUploadTypeFromSession() {
+
+        // lesen
+        Map<String, Object> sessionMAP = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+        String uploadSessionType = (String) sessionMAP.get("uploadSessionType");
+        this.logger.info("Read uploadSessionType... it is a " + uploadSessionType);
+
+        return uploadSessionType;
+    }
+
 
 }
