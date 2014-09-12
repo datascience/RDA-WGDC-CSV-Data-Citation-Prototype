2db7c8a4502abdb117724722bccb9ce227bde5cb
stefanproell <info@stefanproell.at>
2014-09-12 14:53:15 +0200

    record status

--- WebInterface/src/main/java/Bean/DatabaseMigrationController.java
+++ WebInterface/src/main/java/Bean/DatabaseMigrationController.java
@@ -78,6 +78,22 @@
  *    limitations under the License.
  */
 
+/*
+ * Copyright [2014] [Stefan Pröll]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package Bean;
 
 
@@ -209,7 +225,7 @@ public class DatabaseMigrationController implements Serializable {
     private Column[] setColumnAsPrimaryKey(Column[] columns, String primaryKeyColumn) {
         for (Column column : columns) {
             if (column.getColumnName().equals(primaryKeyColumn)) {
-                column.setPrimaryKey();
+
 
             }
         }
