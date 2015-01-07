08c192d90316947a4b43b0c66e1f17422347267e
Stefan Proell <info@stefanproell.at>
2015-01-07 13:54:39 +0100

    initial commit

--- src/main/java/at/stefanproell/PersistentIdentifierMockup/HibernateUtil.java
+++ src/main/java/at/stefanproell/PersistentIdentifierMockup/HibernateUtil.java
@@ -1,35 +1,3 @@
-/*
- * Copyright [2014] [Stefan Pröll]
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
-/*
- * Copyright [2014] [Stefan Pröll]
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
 package at.stefanproell.PersistentIdentifierMockup;
 
 import org.hibernate.HibernateException;
@@ -37,6 +5,10 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * Hibernate session management
@@ -48,7 +20,33 @@ public class HibernateUtil {
     static {
         try {
 
+
+
+            String filename = "db.properties";
+            Properties prop = null;
+
+            prop = Helpers.readPropertyFile(filename);
+
+            String dbhost=prop.getProperty("dbhost");
+            String dbport=prop.getProperty("dbport");
+            String dbname=prop.getProperty("dbname");
+            String dbuser=prop.getProperty("dbuser");
+            String dbpw=prop.getProperty("dbpassword");
+
+            String mysqlString = "jdbc:mysql://" + dbhost + ":"+ dbport+ "/"+ dbname;
+            System.out.println("db string_ " + mysqlString);
+            Properties extraProperties=new Properties();
+            extraProperties.setProperty("hibernate.connection.url",mysqlString);
+            extraProperties.setProperty("hibernate.connection.username",dbuser);
+            extraProperties.setProperty("hibernate.connection.password",dbpw);
+
+
+
             Configuration configuration = new Configuration().setInterceptor(new TimeStampInterceptor());
+            configuration=configuration.configure("hibernate.cfg.xml");
+            configuration=configuration.addProperties(extraProperties);
+
+
             configuration.configure();
 
             serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
@@ -60,6 +58,7 @@ public class HibernateUtil {
             throw new ExceptionInInitializerError(he);
         }
     }
+
 
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
