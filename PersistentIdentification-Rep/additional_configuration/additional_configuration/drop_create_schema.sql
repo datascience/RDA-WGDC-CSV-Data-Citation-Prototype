43864ea834433c878b9b648bf9b459f69ab3da5f
Stefan Proell <info@stefanproell.at>
2015-01-13 13:51:17 +0100

    persistent identification module

--- PersistentIdentification-Rep/additional_configuration/additional_configuration/drop_create_schema.sql
+++
@@ -1,2 +0,0 @@
-DROP DATABASE IF EXISTS querystoreDB;
-CREATE DATABASE querystoreDB;
--- PersistentIdentification-Rep/additional_configuration/additional_configuration/kill_process.sql
+++
@@ -1,7 +0,0 @@
-SHOW FULL PROCESSLIST;
-
-KILL 63;
-
-
-select concat('KILL ',id,';') from information_schema.processlist where user='querystoreuser' into outfile '/tmp/killlist.txt';
-source '/tmp/killlist.txt';
