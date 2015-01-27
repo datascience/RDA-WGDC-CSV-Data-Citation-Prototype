92d3a63021f8fec26c11c87a430f0c239eb081dd
Stefan Proell <info@stefanproell.at>
2015-01-27 15:08:58 +0100

    connections cleaned up

--- additional_configuration/drop_create_schema.sql
+++ additional_configuration/drop_create_schema.sql
@@ -1,2 +1,5 @@
-DROP DATABASE IF EXISTS querystoreDB;
-CREATE DATABASE querystoreDB;
+DROP DATABASE IF EXISTS PersistentIdentifierDB;
+CREATE DATABASE PersistentIdentifierDB;
+
+CREATE USER 'querystoreuser'@'localhost' IDENTIFIED BY 'query2014';
+GRANT ALL PRIVILEGES ON `PersistentIdentifierDB`. * TO 'querystoreuser'@'localhost';
