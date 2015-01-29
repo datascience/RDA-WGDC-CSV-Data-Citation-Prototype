1a04f429d25ca3747c01e648120df8ff966c98af
Stefan Proell <info@stefanproell.at>
2015-01-29 13:45:29 +0100

    pid problem

--- src/main/java/at/stefanproell/Examples/ExamplesAPI.java
+++ src/main/java/at/stefanproell/Examples/ExamplesAPI.java
@@ -21,6 +21,7 @@ public class ExamplesAPI {
 
         // create a dummy organization and provide a prefix
         Organization evilOrganization = api.createNewOrganitation("Evil Corp", 334455);
+        Organization theOrganization = api.createNewOrganitation("The Company", 10101);
         Organization goodOrganization = api.createNewOrganitation("Good Company", 55667);
         // set the length for alphanumeric identifiers
         evilOrganization.setAlphanumericPIDlength(20);
