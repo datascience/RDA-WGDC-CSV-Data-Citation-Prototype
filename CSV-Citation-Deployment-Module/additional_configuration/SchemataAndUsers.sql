7b52642671898b1c1c460c21008934dc9a41df7e
Stefan Proell <info@stefanproell.at>
2015-01-30 14:29:16 +0100

    sorting and filtering

---
+++ CSV-Citation-Deployment-Module/additional_configuration/SchemataAndUsers.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS CitationDB;
+CREATE DATABASE CitationDB;
+DROP DATABASE IF EXISTS QueryStoreDB;
+CREATE DATABASE QueryStoreDB;
+DROP DATABASE IF EXISTS PersistentIdentifierDB;
+CREATE DATABASE PersistentIdentifierDB;
+DROP DATABASE IF EXISTS CitationUserDB;
+CREATE DATABASE CitationUserDB;
+
+
+DROP USER 'querystoreuser';
+FLUSH PRIVILEGES;
+CREATE USER 'querystoreuser'@'localhost' IDENTIFIED BY 'query2014';
+
+
+
+GRANT ALL PRIVILEGES ON `QueryStoreDB`. * TO 'querystoreuser'@'localhost';
+GRANT ALL PRIVILEGES ON `CitationUserDB`. * TO 'querystoreuser'@'localhost';
+GRANT ALL PRIVILEGES ON `CitationDB`. * TO 'querystoreuser'@'localhost';
+GRANT ALL PRIVILEGES ON `PersistentIdentifierDB`. * TO 'querystoreuser'@'localhost';
+GRANT ALL PRIVILEGES ON `CitationUserDB`. * TO 'querystoreuser'@'localhost';
+
+FLUSH PRIVILEGES;
+
+
