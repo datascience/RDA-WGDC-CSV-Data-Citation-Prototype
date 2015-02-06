f10c926466c82a5766ce97a0626ba1ebe9e62f02
Stefan Proell <info@stefanproell.at>
2015-02-06 09:39:53 +0100

    resolving conflicts

---
+++ Database/SQL-Examples/UserData.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM CitationUserDB.user
+LIMIT 0, 1000
+
+-- Date: 2015-01-29 09:24
+*/
+INSERT INTO `CitationUserDB`.`user` (`user_id`,`organizational_id`,`password`,`username`) VALUES (1,12345,'$2a$12$cGvczaMlQKYW2cxIIcTBcu7IhqAg.cZ2gZC3VVMCMoD8Ta41Y.bTy','carl');
+INSERT INTO `CitationUserDB`.`user` (`user_id`,`organizational_id`,`password`,`username`) VALUES (2,12345,'$2a$12$cef4kxW.Gs/eGLlxpGaAYOMKQ4oDTbwG9QfxTdtAEelXMMZMqTD5q','stefan');
+INSERT INTO `CitationUserDB`.`user` (`user_id`,`organizational_id`,`password`,`username`) VALUES (3,12345,'$2a$12$MdZoEq9pnXzufZtDFnMX5.JZfTwgKFyPh8IVlkuzUs3CsKfXWl/IC','andi');
