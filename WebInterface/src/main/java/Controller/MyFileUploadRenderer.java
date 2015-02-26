5c05afecdd128b70fa2ac4c4d8eed6d0c84c742f
Stefan Proell <info@stefanproell.at>
2015-02-26 15:41:42 +0100

    file upload problem solved. works with one button now

---
+++ WebInterface/src/main/java/Controller/MyFileUploadRenderer.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright [2015] [Stefan Pröll]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/*
+ * Copyright [2015] [Stefan Pröll]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package Controller;
+
+/**
+ * CSV-DataCitation
+ * Created by stefan
+ * {MONTH_NAME_FULL} {YEAR}
+ */
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.component.fileupload.FileUploadRenderer;
+
+public class MyFileUploadRenderer extends FileUploadRenderer {
+
+    @Override
+    public void decode(FacesContext context, UIComponent component) {
+        if (context.getExternalContext().getRequestContentType().toLowerCase().startsWith("multipart/")) {
+            super.decode(context, component);
+        }
+    }
+
+}
