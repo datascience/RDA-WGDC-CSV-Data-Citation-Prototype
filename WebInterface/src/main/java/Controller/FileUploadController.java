cec75e2efaf20189bcca7cb794c6ac70e13654bc
Stefan Proell <info@stefanproell.at>
2015-03-02 16:08:07 +0100

    file upload adapted. now only one form

--- WebInterface/src/main/java/Controller/FileUploadController.java
+++ WebInterface/src/main/java/Controller/FileUploadController.java
@@ -47,6 +47,22 @@
  */
 
 /*
+ * Copyright [2015] [Stefan Pröll]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/*
  * Copyright [2014] [Stefan Pröll]
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
@@ -573,5 +589,13 @@ public class FileUploadController implements Serializable {
 
     public void setUploadedCSVFile(File uploadedCSVFile) {
         this.uploadedCSVFile = uploadedCSVFile;
+    }
+
+    public void updateTableNameInput(){
+        SessionManager sm = new SessionManager();
+        User user = sm.getLogedInUserObject();
+        this.tableNameInput = "csv_"+user.getUsername()+"_"+this.dataSetTitle.replaceAll(" ", "_").toLowerCase();
+
+
     }
 }
