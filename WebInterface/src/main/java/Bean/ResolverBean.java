b2b38b390b10236dc467cbc21c29c7295a1233f5
Stefan Proell <info@stefanproell.at>
2015-03-06 15:36:00 +0100

    resolver

--- WebInterface/src/main/java/Bean/ResolverBean.java
+++ WebInterface/src/main/java/Bean/ResolverBean.java
@@ -30,6 +30,22 @@
  *    limitations under the License.
  */
 
+/*
+ * Copyright [2015] [Stefan Pröll]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package Bean;
 
 import Controller.ResolverController;
@@ -69,6 +85,8 @@ public class ResolverBean implements Serializable{
     
     private ResolverController resolverController;
     private String requestPID;
+    private String resolveInputPID;
+    
     
     
 
@@ -209,6 +227,13 @@ public class ResolverBean implements Serializable{
         
     }
 
+    public void resolvePIDfromTextInput(){
+        this.logger.info("Resolving: " + this.requestPID);
+        this.resolverController.resolvePID(this.requestPID);
+
+
+    }
+
     public List<SelectItem> getAvailableSubsets() {
         return availableSubsets;
     }
@@ -236,5 +261,13 @@ public class ResolverBean implements Serializable{
 
     public void setRequestPID(String requestPID) {
         this.requestPID = requestPID;
+    }
+
+    public String getResolveInputPID() {
+        return resolveInputPID;
+    }
+
+    public void setResolveInputPID(String resolveInputPID) {
+        this.resolveInputPID = resolveInputPID;
     }
 }
