0960367ecd39b7702fc9fe9c0a6680710f78ae29
Stefan Pröll <sproell@sba-research.org>
2015-12-03 11:20:55 +0100

    CSV test data generator

---
+++ Evaluation-Module/src/main/java/at/stefanproell/CSV_Testdata_Generator/GenerateTestDataMain.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright [2015] [Stefan Pröll]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package at.stefanproell.CSV_Testdata_Generator;
+
+/**
+ * CSV-DataCitation
+ * Created by stefan
+ * {MONTH_NAME_FULL} {YEAR}
+ */
+public class GenerateTestDataMain {
+
+    public static void main(String[] args) {
+
+        int averageStringLength=5;
+        double variance = 2.0;
+        int amountOfRecords = 10;
+        String[] headers = {"Column_1","Column_2","Column_3","Column_4","Column_5"};
+
+        CSV_Testdata_Writer csv = new CSV_Testdata_Writer("/tmp/testfile.csv",headers,amountOfRecords,
+                averageStringLength,variance);
+
+
+
+
+
+
+    }
+}
